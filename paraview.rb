5f10eb6de2a32230337282a14aa7ba2735dc456d
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2014-10-27 20:57:37 -0400

    Updating based on feedback.
    1. Using homebrew depenencies.
    2. Using one-line if conditions
    3. Removed redundant comments.

--- paraview.rb
+++ paraview.rb
@@ -6,59 +6,48 @@ class Paraview < Formula
   version '4.2.0'
   sha1 '77cf0e3804eb7bb91d2d94b10bd470f4'
 
-  # 'head' release URL
   head 'git://paraview.org/ParaView.git', :using => :git, :tag => 'master'
 
   depends_on 'cmake' => :build
 
-  # Core dependencies. If any of these are off, they are OFF!
-  depends_on :mpi => [:cc, :cxx, :optional] # Is optional. MPI with `cc` and `cxx`.
-  depends_on :python => :recommended
-  depends_on 'qt' => :recommended
-  depends_on 'ffmpeg' => :recommended
-  depends_on 'cgns' => :recommended
   depends_on 'boost' => :recommended
+  depends_on 'cgns' => :recommended
+  depends_on 'ffmpeg' => :recommended
+  depends_on 'qt' => :recommended
+  depends_on :mpi => [:cc, :cxx, :optional]
+  depends_on :python => :recommended
 
-  # Python module dependencies
   depends_on 'matplotlib' => [:python, :optional]
 
-  # Builtin dependencies. If any of these are off, we build our own.
-  depends_on :libpng => :recommended
-  depends_on :freetype => :optional
-  depends_on :fontconfig => :optional
-  depends_on 'jpeg' => :optional
-  depends_on 'libtiff' => :optional
-  depends_on 'hdf5' => :recommended
+  depends_on 'freetype'
+  depends_on 'hdf5'
+  depends_on 'jpeg'
+  depends_on 'libtiff'
+  depends_on :fontconfig
+  depends_on :libpng
 
   def install
     args = std_cmake_args + %W[
       -DBUILD_SHARED_LIBS=ON
       -DBUILD_TESTING=OFF
-      -DVTK_USE_SYSTEM_EXPAT=ON
-      -DVTK_USE_SYSTEM_LIBXML2=ON
-      -DVTK_USE_SYSTEM_ZLIB=ON
-      -DPARAVIEW_DO_UNIX_STYLE_INSTALLS:BOOL=OFF
       -DMACOSX_APP_INSTALL_PREFIX:PATH=#{prefix}
+      -DPARAVIEW_DO_UNIX_STYLE_INSTALLS:BOOL=OFF
+      -DVTK_USE_SYSTEM_EXPAT:BOOL=ON
+      -DVTK_USE_SYSTEM_FREETYPE:BOOL=ON
+      -DVTK_USE_SYSTEM_HDF5:BOOL=ON
+      -DVTK_USE_SYSTEM_JPEG:BOOL=ON
+      -DVTK_USE_SYSTEM_LIBXML2:BOOL=ON
+      -DVTK_USE_SYSTEM_PNG:BOOL=ON
+      -DVTK_USE_SYSTEM_TIFF:BOOL=ON
+      -DVTK_USE_SYSTEM_ZLIB:BOOL=ON
     ]
 
-    # enable/disable Qt support
-    if build.with? 'qt'
-      args << '-DPARAVIEW_BUILD_QT_GUI:BOOL=ON'
-    else
-      args << '-DPARAVIEW_BUILD_QT_GUI:BOOL=OFF'
-    end
-
+    args << "-DPARAVIEW_BUILD_QT_GUI:BOOL=OFF" if !build.with? 'qt'
     args << '-DPARAVIEW_USE_MPI:BOOL=ON' if build.with? :mpi
     args << '-DPARAVIEW_ENABLE_FFMPEG:BOOL=ON' if build.with? 'ffmpeg'
-    args << '-DVTK_USE_SYSTEM_FREETYPE=ON' if build.with? :freetype
-    args << '-DVTK_USE_SYSTEM_HDF5=ON' if build.with? 'hdf5'
-    args << '-DVTK_USE_SYSTEM_JPEG=ON' if build.with? 'jpeg'
-    args << '-DVTK_USE_SYSTEM_PNG=ON' if build.with? :libpng
-    args << '-DVTK_USE_SYSTEM_TIFF=ON' if build.with? 'libtiff'
     args << '-DPARAVIEW_USE_VISITBRIDGE:BOOL=ON' if build.with? 'boost'
     args << '-DVISIT_BUILD_READER_CGNS:BOOL=ON' if build.with? 'cgns'
     mkdir 'build' do
-      # enable/disable Python support
       if build.with? 'python'
         args << '-DPARAVIEW_ENABLE_PYTHON:BOOL=ON'
         # CMake picks up the system's python dylib, even if we have a brewed one.
